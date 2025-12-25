4e64d90aef0cc32800e76ee48c8d5f963f770cce
Ksireon <ksireon.007@gmail.com>
2025-12-25 17:15:47 +0500

    resolvet

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,7 @@ import { CartModule } from './cart/cart.module'
 import { OrdersModule } from './orders/orders.module'
 import { ProductsModule } from './products/products.module'
 import { HealthModule } from './health/health.module'
-<<<<<<< HEAD
 import { SystemModule } from './system/system.module'
-=======
-import { BranchesModule } from './branches/branches.module'
-import { CategoriesModule } from './categories/categories.module'
-import { SettingsModule } from './settings/settings.module'
->>>>>>> 201d046b37daf4793c8f1eb0498a970ad213f00b
 
 @Module({
   imports: [
@@ -28,13 +22,7 @@ import { SettingsModule } from './settings/settings.module'
     OrdersModule,
     ProductsModule,
     HealthModule,
-<<<<<<< HEAD
     SystemModule
-=======
-    BranchesModule,
-    CategoriesModule,
-    SettingsModule
->>>>>>> 201d046b37daf4793c8f1eb0498a970ad213f00b
   ],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }]
 })
